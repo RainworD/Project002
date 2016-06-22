faae92dbcd49354995cdd1f6981740a3d2a0aaa6
Holly <673227139@qq.com>
2016-06-22 20:11:39 +0800

    点赞+图文显示做成功

--- source/js/barselect.js
+++ source/js/barselect.js
@@ -40,7 +40,7 @@ mui.init();
 						for (var i = 0; i < count; i++) {
 							li = document.createElement('li');
 							li.className = 'mui-table-view-cell';
-							li.innerHTML = '第' + (index + 1) + '个选项卡子项-' + (length + (reverse ? (count - i) : (i + 1)));
+							li.innerHTML = '<div class="item-content-whole"><p class="item-vedio-title">军方：中国军舰正常通过日临近海域 符合国际法</p><p class="article-sorts">政策法规&nbsp;中国银监会&nbsp;203评论&nbsp;400赞</p></div>';
 							fragment.appendChild(li);
 						}
 						return fragment;
